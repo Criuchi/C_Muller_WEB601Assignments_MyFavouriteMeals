30558c042a351ac54c03c4a8fbbdd7e68326d7c7
Cristian Hernan Muller <121682366+Criuchi@users.noreply.github.com>
2024-02-01 19:56:49 -0300

    Removing content-list helper file and Creation of Content-List component
    Deleted all the content and files required and created a new component with an array included in it.

--- C_Muller_MyFavoriteMeals/src/app/content-card/content-card.component.ts
+++ C_Muller_MyFavoriteMeals/src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ContentList } from '../helper-files/content-list';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,36 +10,7 @@ import { CommonModule } from '@angular/common';
 })
 
 export class ContentCardComponent implements OnInit {
-  contentList:ContentList;
 
-  // Creation of the contructor to add 3 new items
-  constructor() {
-    this.contentList = new ContentList;
-    this.contentList.addContentItem({
-      id: 1,
-      title: 'Tonkatsu',
-      description: 'Famous Japanese meal which consists of breaded pork, rice and vegetables',
-      creator: 'Sushi Jo',
-      type: 'Complete Meal',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Kerrie_rijst_met_tonkatsu_%28gefrituurde_varkenskoteletten%29_en_salade_in_het_restaurantje_%E2%80%9EPond%E2%80%9D_in_K%C5%8Dt%C5%8D-ku%2C_-20_november_2019.jpg',
-    });
-    this.contentList.addContentItem({
-      id: 2,
-      title: 'Caesar Salad',
-      description: 'Fresh mix of vegatebales, chicken and croutons',
-      creator: 'Griff Cavern',
-      type: 'Light Meal',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/CaesarSalad3.jpg',
-    });
-    this.contentList.addContentItem({
-      id: 3,
-      title: 'Empanadas',
-      description: 'A famous plate from Argentina, consisting in a fried dough filled with ground beef and vegetables',
-      creator: 'Cristian Muller',
-      type: 'Full Meal',
-      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Empanadas_cr.jpg',
-    });
-  }
 
   ngOnInit(){
     
---
+++ C_Muller_MyFavoriteMeals/src/app/content-list/content-list.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Content } from '../helper-files/content-interface';
+import {ContentCardComponent} from '../content-card/content-card.component';
+
+@Component({
+  selector: 'app-content-list',
+  standalone: true,
+  imports: [CommonModule, ContentCardComponent],
+  templateUrl: './content-list.component.html',
+  styleUrl: './content-list.component.scss'
+})
+export class ContentListComponent {
+  contentArray: Content[] = [
+    {
+      id: 1,
+      title: 'Tonkatsu',
+      description: 'Famous Japanese meal which consists of breaded pork, rice and vegetables',
+      creator: 'Sushi Jo',
+      type: 'Complete Meal',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Kerrie_rijst_met_tonkatsu_%28gefrituurde_varkenskoteletten%29_en_salade_in_het_restaurantje_%E2%80%9EPond%E2%80%9D_in_K%C5%8Dt%C5%8D-ku%2C_-20_november_2019.jpg',
+      tags: ['Japanese, ', 'Tasty, ', 'Healthy'],
+    },
+    {
+      id: 2,
+      title: 'Caesar Salad',
+      description: 'Fresh mix of vegatebales, chicken and croutons',
+      creator: 'Griff Cavern',
+      type: 'Light Meal',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/CaesarSalad3.jpg',
+      tags: ['Light, ', 'Salad, ', 'Healthy'],
+    },
+    {
+      id: 3, 
+      title: 'Empanadas',
+      description: 'A famous plate from Argentina, consisting in a fried dough filled with ground beef and vegetables',
+      creator: 'Cristian Muller',
+      type: 'Soup',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/b/b7/Empanadas_cr.jpg',
+      tags: ['Japanese, ', 'Tasty, ', 'Soup'],
+    },
+    {
+      id: 4, 
+      title: 'Ramen',
+      description: 'pork, eggs and vegetables cooked in a broth',
+      creator: 'Trinity Scott',
+      type: 'Complete Meal',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Hachioji.jpg',
+      tags: ['Japanese, ', 'Tasty, ', 'Healthy'],
+    },
+    {
+      id: 5,
+      title: 'Fish Tacos',
+      description: 'Tacos with a new addition of fried fish',
+      creator: 'Monica Galindo',
+      type: 'Complete Meal',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/1/1e/Tacos_de_pescado_Ensenada..jpg',
+      tags: ['Mexican, ', 'Tasty, ', 'Delicious'],
+    },
+    {
+      id: 6, 
+      title: 'Gnocci',
+      description: 'Home made gnocci along with a delicious tomato sauce',
+      creator: 'Andrea Sisto',
+      type: 'Complete Meal',
+      imgURL: 'https://upload.wikimedia.org/wikipedia/commons/d/dd/Kerrie_rijst_met_tonkatsu_%28gefrituurde_varkenskoteletten%29_en_salade_in_het_restaurantje_%E2%80%9EPond%E2%80%9D_in_K%C5%8Dt%C5%8D-ku%2C_-20_november_2019.jpg',
+      tags: ['Italian, ', 'Tasty, ', 'Healthy'],
+    },
+
+  ]
+
+  constructor() {
+  
+  }
+}
