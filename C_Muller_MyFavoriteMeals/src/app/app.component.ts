27d7ffd4e768ea83f98ec15b727fcb7fd017781a
Cristian Hernan Muller <121682366+Criuchi@users.noreply.github.com>
2024-03-11 15:45:54 -0300

    Progress part 1

--- C_Muller_MyFavoriteMeals/src/app/app.component.ts
+++ C_Muller_MyFavoriteMeals/src/app/app.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import {Content} from './helper-files/content-interface';
+import { MealServiceService } from './meal-service.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContentCardComponent, ContentListComponent],
+  imports: [RouterOutlet, ContentCardComponent, ContentListComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'C_Muller_MyFavoriteMeals';
+
+  
+  customContent: Content | undefined;
+
+  constructor(private mealService: MealServiceService) {}
+
+  ngOnInit(): void {
+    
+    this.getContentById(1); 
+  }
+
+  getContentById(id: number): void {
+    this.mealService.getContentById(id).pipe(
+      tap((content: Content | undefined) => {
+        this.customContent = content;
+      }),
+      
+      catchError((error) => {
+        console.error('Error fetching content by ID:', error);
+        return of(null);
+      })
+    )
+
+    .subscribe();
+  }
 }
